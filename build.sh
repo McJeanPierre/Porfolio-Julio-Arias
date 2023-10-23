42ae1481b88771be6ade21e5c468616029c42a1b
Jean Pierre Moreira Palacios <jeanpierremoreirapalacios@gmail.com>
2023-10-23 13:20:50 -0500

    third commit

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 # poetry install 
 pip install -r requirements.txt
 
+DJANGO_SUPERUSER_PASSWORD=12345 python manage.py createsuperuser --no-input --username=Julio  --email=joe@example.com
 
 python manage.py collectstatic --no-input
 python manage.py migrate
